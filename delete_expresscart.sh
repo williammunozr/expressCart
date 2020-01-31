9f6b7ef2151d262bc9764c11b9b5c4196dbfb6bd
William MR <williammr@Williams-MacBook-Pro.local>
2020-01-31 10:22:40 -0500

    Google Cloud Auth Configuration

--- delete_expresscart.sh
+++ delete_expresscart.sh
@@ -2,7 +2,8 @@
 
 cd kubernetes/
 
-gcloud container clusters get-credentials gke-expresscart --region us-central1 --project gke-terraform-2508
+# GKE Authentication
+gcloud container clusters get-credentials gke-expresscart --region us-central1 --project kubernetes-2020
 
 kubectl delete -f expresscart_service.yaml
 
